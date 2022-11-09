8c4b5ea1151cbf0e3cba103c566d013462e47a6d
tutulportal <tutulvpn3@gmail.com>
2022-11-09 08:31:17 +0600

    added mongodb config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,54 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+// setup mongodb
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.lbao5mz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async() => {
+    try{
+        const serviceCollection = client.db('CoffeeTreat').collection('services');
+
+        // read limited number data from mongodb
+        app.get('/services/:count', async(req, res) => {
+            const num = parseInt(req.params.count);
+            const query = {};
+            const cursor = serviceCollection.find(query).sort({ _id: -1 }).limit(num);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // read all data from mongodb
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // write data to mongodb
+        // app.post('/users', async(req, res) => {
+        //     const user = req.body;
+        //     console.log(user);
+        //     const result = await userCollection.insertOne(user);
+        //     res.send(result);
+        // })
+
+        // // delete mongodb data
+        // app.delete('/users/:id', async(req, res) => {
+        //     const id = req.params.id;
+        //     const query = {_id: ObjectId(id)};
+        //     const result = await userCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
